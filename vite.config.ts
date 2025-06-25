490956799c49186db41c394aa3bc4f84ca4b4884
Joshbond123 <adeniranj787@gmail.com>
2025-06-25 12:11:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { viteStaticCopy } from "vite-plugin-static-copy"; // ✅ NEW LINE
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,15 +11,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects", // ✅ COPY THIS FILE
-          dest: ".",                // ✅ TO dist/
-        },
-      ],
-    }),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
